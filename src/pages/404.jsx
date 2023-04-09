12e5cc4257fcbe2aef49e4dfdcedb650fff86c72
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2023-04-09 03:19:54 +0100

    completed 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import "@styles/four04.css";
 
 const Four04 = () => {
-  return (
-    <div className='four04'>Four04</div>
-  )
+    return (
+        <div className='four04'>
+            <div className="four04_innerWrapper">
+                <div className="four04_overlay">
+                    <p className="smallTxt">You're Lost</p>
+                    <p className="four04_txt">404</p>
+                    <div className="four04cta">
+                        <p>Return to shop</p>
+                        <hr className="four04Underline" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Four04;
